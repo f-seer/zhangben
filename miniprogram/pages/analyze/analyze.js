4cd3e9a4361b1ae0ee150bc6317033b816a16f24
ipanda123 <1054261856@qq.com>
2020-07-23 07:56:58 +0800

    analyse1

--- miniprogram/pages/analyze/analyze.js
+++ miniprogram/pages/analyze/analyze.js
@@ -1,13 +1,39 @@
 // pages/analyze/analyze.js
+import { echarts } from './echarts.min.js';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    info:[{itemname: "显示名称",
+    itemnum: "显示编号",
+    pamount: "进货数量",
+    purc: "进货价格",
+    sell: "默认售价",
+    sellamount: 1}],
+    sum: 0
   },
 
+
+  
+  
+  showData: function() {
+    var Info =wx.getStorageSync('info');
+    this.setData({
+      info:Info
+    })
+    var i=this.data.info.length-1;
+    var s=0;
+    for(i;i>=0;i--)
+    {
+      s+=this.data.info[i].sell*this.data.info[i].sellamount;
+    }
+    this.setData({
+      sum:s
+    })
+ },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,7 +52,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.showData()
   },
 
   /**
@@ -62,5 +88,35 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  
+
+  chart_1:function(){
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: '销量统计表'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: this.data.info.itemname
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: this.data.info.pamount
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+      }
+
 })
